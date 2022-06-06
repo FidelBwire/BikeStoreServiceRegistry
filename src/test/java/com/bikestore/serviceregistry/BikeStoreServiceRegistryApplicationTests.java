ff0d9eaecb595c93f4b26faa69977382cb907455
Fidelis Bwire <fidelisbwire189@gmail.com>
2022-06-06 22:37:20 +0300

    Initial service registry commit

---
+++ src/test/java/com/bikestore/serviceregistry/BikeStoreServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikestore.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeStoreServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
